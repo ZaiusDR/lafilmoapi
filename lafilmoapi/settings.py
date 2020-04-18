c975d1be3f559b430e92233d6adf7a5ed4d1e84c
Eduardo Suarez <eduardosuarez@gmail.com>
2020-04-18 20:49:06 +0200

    Adding Heroku vhost to allowed hosts

--- lafilmoapi/settings.py
+++ lafilmoapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='mysupersecretkey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lafilmoapi.herokuapp.com']
 
 
 # Application definition
